e6f0289d73293fd3540b7b3b5e469a9a7402e005
Seiichi Ariga <s-ariga@sanctus.jp>
2019-06-30 02:37:18 +0900

    Problem 35

---
+++ python/problem35/problem35.py
@@ -0,0 +1,24 @@
+﻿#!/usr/bin/env python
+# -*- coding : utf-8 -*-
+
+import math
+import ..scripts.epcommon as ec
+
+total_circular_primes = 0
+
+def circular(n : int):
+    '''
+    数値nの桁を入れ替えた数値のリストを返す
+    
+    parameters
+    ------------
+    n:int
+    
+    returns
+    -----------
+    int[]
+    '''
+    n_str = str(n)
+    
+    
+        
